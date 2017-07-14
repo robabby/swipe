5767f86c71aaf72fb7a1a8c8d69e4dc60e04782f
robabby <robabby23@gmail.com>
2017-07-13 19:27:58 -0500

    Added react-native-elements and added components to the Cards

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   Text,
   View
 } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 import Deck from './src/Deck.js'
 
 const DATA = [
@@ -20,9 +21,19 @@ const DATA = [
 export default class App extends React.Component {
   renderCard(item) {
     return (
-      <Text>
-        {item.text}
-      </Text>
+      <Card
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text stye={{ marginBottom: 10 }}>
+          I can customize the card further
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
     )
   }
   render() {
